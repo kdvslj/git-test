c310b836c2ea63b5178a18ad870b0fd392805c30
LiZhu <lizhu960912@gmail.com>
2021-08-18 19:41:37 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author lizhu
+ * @create 2021-08-18 19:38
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
